566c5a994bcc8217870204dcd4d6aefec2baebe5
spectrumtouch <spectrum_touch@yahoo.com>
2016-11-08 00:53:54 -0500

    -Layout for Basis Set Selection

--- Schroedinger/Program.cs
+++ Schroedinger/Program.cs
@@ -9,18 +9,44 @@ namespace Schroedinger
     class SchroedingerPgm
     {
         /// Define the potential energy V(X)
-        
+
         /// Define c the constant
-        
+
         /// Define the size of the basis set
-        
+
         /// Define the choice of basis set function
         
+        public int[] BasisSet(int choice)
+        {
+            int[] Basis = new int[2];
+
+
+            ///Prompt user to pick basis set
+            
+            choice = Console.Read();
+
+            if (choice == 1)
+            {
+                Basis = new int[2]
+
+                { 1, 3};
+            }
+            else if (choice==2)
+            {
+                Basis = new int[2] 
+
+                { 2, 4};
+            }
+
+            return Basis;
+        }
+
         /// Define Wave Function
         public double WaveFunction(double B, int n, float x)
         {
             return B*Math.Sin(n*x*Math.PI);
         }
+
         static void Main(string[] args)
         {
             int n = 1; ///Depends on energy level
