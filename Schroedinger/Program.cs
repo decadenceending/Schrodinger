611d83cc3249253ecd0142352f434d31fea0bab3
spectrumtouch <spectrum_touch@yahoo.com>
2016-11-07 22:55:27 -0500

    -Initialization

--- Schroedinger/Program.cs
+++ Schroedinger/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Schroedinger
 {
-    class Program
+    class SchroedingerPgm
     {
+        /// Define the potential energy V(X)
+        
+        /// Define c the constant
+        
+        /// Define the size of the basis set
+        
+        /// Define the choice of basis set function
+        
+        /// Define Wave Function
+        public double WaveFunction(float B, int n, float x)
+        {
+            return B*Math.Sin(n*x*Math.PI);
+        }
         static void Main(string[] args)
         {
+            int y = 2;
+            double x = new SchroedingerPgm().WaveFunction(y);
         }
     }
 }
