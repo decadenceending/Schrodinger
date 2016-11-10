c8163624c3c12bd4f329e78e9283991f2e2315e4
spectrumtouch <spectrum_touch@yahoo.com>
2016-11-09 23:42:59 -0500

    -Legendre Polynomial Coefficients

--- Schroedinger/Program.cs
+++ Schroedinger/Program.cs
@@ -8,14 +8,21 @@ namespace Schrodinger
 {
     class SchrodingerPgm
     {
-        /// Define the potential energy V(X)
+        /// Define the potential energy V_0
+
+        double V_0 = 1.5;
 
         /// Define c the constant
 
-        /// Define the size of the basis set
+        double c;
+
+        /// Define Legendre polynomial coefficients
+
+        int[] basis_legendre = { 1, 1, -1, 3, -3, 5, 3, -30, 35, 15, -70, 63, -5, 105, -315, 231, -35, 315, -693, 429, 35, -1260, 6930, -12012, 6435, 315,
+            -4620, 18018, -25740, 12155, -63, 3465, -30030, 90090, -109395, 46189, -693, 15015, -90090, 218790, -230945, 88179, 231, -18018, 225225, -1021020, 2078505, -1939938, 676039 };
 
         /// Define the choice of basis set function
-        
+
         public int[] BasisSet()
         {
             int[] Basis = new int[2];
@@ -25,6 +32,14 @@ namespace Schrodinger
 
             Console.WriteLine("Please select your basis set of choice: 1 for Legendre, 2 for Fourier:");
             int choice = Convert.ToInt32(Console.ReadLine());
+
+            /// Define the size of the basis set
+
+            Console.WriteLine("Please speciofy the number of terms for your basis set:");
+            int bsize = Convert.ToInt32(Console.ReadLine());
+
+            ///Keep Console open, until user chooses to close it
+
             Console.ReadLine();
 
             if (choice == 1)
@@ -44,17 +59,27 @@ namespace Schrodinger
         }
 
         /// Define Wave Function
-        public double WaveFunction(double B, int n, float x)
+        public double WaveFunction(double B, int n, double x)
         {
             return B*Math.Sin(n*x*Math.PI);
         }
 
+        ///Define Hamiltonian
+        public void Hamiltonian()
+        {
+            return -c * Laplacian + V_0;
+        }
+
         static void Main(string[] args)
         {
-            int n = 1; ///Depends on energy level
-            double B = (Math.PI) / 2; ///For Example
-            float x = 2;
-            double Psi = new SchrodingerPgm().WaveFunction(B,n,x);
+            ///Depends on energy level int n = 1;
+            ///double B = (Math.PI) / 2;
+            ///float x = 2;
+            ///double Psi = new SchrodingerPgm().WaveFunction(B,n,x);
+
+            ///Call the functions and perform the operations to find the result
+            
+
         }
     }
 }
